749c8a4c33ea37877d05e9836d00abd7e948d1a6
leandroxelor00 <leandroxelor00@gmail.com>
2026-09-11 10:57:47 -0300

    feat: Stroop Test game finalized

--- frontend/src/games/stroop-test/StroopTest.jsx
+++ frontend/src/games/stroop-test/StroopTest.jsx
@@ -1,21 +1,82 @@
 import { useEffect, useState } from "react";
 import { UseStroopTest } from "./useStroopTest";
-import "./StroopTest.module.css";
+import styles from "./StroopTest.module.css";
 
 export function StroopTest() {
-  const { timer, score, startTimer, currentWord, currentColor, changeWord, isColorBlue,isColorGreen,isColorRed,isColorYellow } = UseStroopTest();
+  const {
+    timer,
+    score,
+    startTimer,
+    currentWord,
+    currentColor,
+    changeWord,
+    colorCorrect,
+    avgReactionTime,
+  } = UseStroopTest();
 
   return (
-    <div style={{display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center"}}>
-      <p>Tempo: {timer}</p>
-      <p>Pontuação: {score}</p>
-      <p style={{color: currentColor}}>{currentWord} </p>
-      
-      <div id="btn-container" style={{display: "grid", gridTemplateColumns: "1fr 1fr", gap: 5}}>
-      <button className="btncolor" onClick={()=> {isColorBlue();changeWord();startTimer()}}>AZUL</button>
-      <button className="btncolor" onClick={()=> {isColorGreen();changeWord();startTimer()}}>VERDE</button>
-      <button className="btncolor" onClick={()=> {isColorRed();changeWord();startTimer()}}>VERMELHO</button>
-      <button className="btncolor" onClick={()=> {isColorYellow();changeWord();startTimer()}}>AMARELO</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <p>Stroop Test</p>
+      <div className={styles.infoContainer}>
+        <p>Tempo: {timer}</p>
+        <p>Pontuação: {score}</p>
+        <p className={styles.reactionTime}>
+          Tempo de reação (média): {avgReactionTime}ms
+        </p>
+      </div>
+      <p style={{ color: currentColor }}>{currentWord} </p>
+
+      <div
+        id="btn-container"
+        style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: 5 }}
+      >
+        <button
+          className="btncolor"
+          onClick={() => {
+            colorCorrect("AZUL");
+            changeWord();
+            startTimer();
+          }}
+        >
+          AZUL
+        </button>
+        <button
+          className="btncolor"
+          onClick={() => {
+            colorCorrect("VERDE");
+            changeWord();
+            startTimer();
+          }}
+        >
+          VERDE
+        </button>
+        <button
+          className="btncolor"
+          onClick={() => {
+            colorCorrect("VERMELHO");
+            changeWord();
+            startTimer();
+          }}
+        >
+          VERMELHO
+        </button>
+        <button
+          className="btncolor"
+          onClick={() => {
+            colorCorrect("AMARELO");
+            changeWord();
+            startTimer();
+          }}
+        >
+          AMARELO
+        </button>
       </div>
     </div>
   );
--- frontend/src/games/stroop-test/useStroopTest.js
+++ frontend/src/games/stroop-test/useStroopTest.js
@@ -1,4 +1,5 @@
-import { use, useEffect, useState } from "react";
+// @ts-nocheck
+import { useRef, useEffect, useState } from "react";
 
 export function UseStroopTest() {
   const [score, setScore] = useState(0);
@@ -7,15 +8,19 @@ export function UseStroopTest() {
   const [gameOver, setGameOver] = useState(false);
 
   const words = ["AZUL", "AMARELO", "VERMELHO", "VERDE"];
-  const colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00"];
+  const colors = ["#FF0000", "#00FF00", "#0000FF", "#EBE134"];
 
   const [currentWord, setCurrentWord] = useState(words[0]);
   const [currentColor, setCurrentColor] = useState(colors[0]);
+
+  const [reactionTimes, setReactionTimes] = useState([]);
+  const startTime = useRef(performance.now());
 
   useEffect(() => {
     if (timer === 0) {
       setGameOver(true);
       setIsTimerOn(false);
+      setTimer(0);
 
       alert("O jogo acabou!");
       alert(`Pontuação: ${score}`);
@@ -46,42 +51,42 @@ export function UseStroopTest() {
   }
 
   function changeWord() {
+    startTime.current = performance.now();
     setCurrentWord(words[Math.floor(Math.random() * words.length)]);
 
     if (currentWord === "VERMELHO") {
       removeColor("#FF0000");
     }
     if (currentWord === "VERDE") {
-      removeColor("#00FF00F");
+      removeColor("#00FF00");
     }
     if (currentWord === "AZUL") {
       removeColor("#0000FF");
     }
     if (currentWord === "AMARELO") {
-      removeColor("#FFFF00");
+      removeColor("#EBE134");
     }
   }
 
-  function isColorBlue() {
-    if (currentWord === "AZUL") {
+  function colorCorrect(color) {
+    const timeSpend = performance.now() - startTime.current;
+    setReactionTimes((reactionTimes) => [...reactionTimes, timeSpend]);
+    if (gameOver) {
+      return;
+    }
+
+    if (currentWord === color) {
       setScore((score) => score + 1);
     }
   }
-  function isColorGreen() {
-    if (currentWord === "VERDE") {
-      setScore((score) => score + 1);
-    }
-  }
-  function isColorRed() {
-    if (currentWord === "VERMELHO") {
-      setScore((score) => score + 1);
-    }
-  }
-  function isColorYellow() {
-    if (currentWord === "AMARELO") {
-      setScore((score) => score + 1);
-    }
-  }
+
+  const sumReactionTime = reactionTimes.reduce(
+    (a, reactionTime) => a + reactionTime,
+    0
+  );
+
+  const avg = sumReactionTime / reactionTimes.length;
+  const avgReactionTime = Number(avg.toFixed(2));
 
   return {
     timer,
@@ -90,9 +95,7 @@ export function UseStroopTest() {
     currentWord,
     currentColor,
     changeWord,
-    isColorBlue,
-    isColorGreen,
-    isColorRed,
-    isColorYellow,
+    colorCorrect,
+    avgReactionTime,
   };
 }
